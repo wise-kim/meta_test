87aea03a7e128917ced8a84e882f5bfd2e2c3375
wise-kim <wisekim@ihopper.co.kr>
2022-12-27 11:46:12 +0900

    'hosting'

--- dist/assets/btn_naver-0213b6ac.js
+++
@@ -1,1 +0,0 @@
-const t=""+new URL("../imgs/btn_kakao.svg",import.meta.url).href,r=""+new URL("../imgs/btn_naver.png",import.meta.url).href;export{t as _,r as a};
---
+++ dist/assets/btn_naver-a459f51d.js
@@ -0,0 +1,1 @@
+const t="/meta_test/imgs/btn_kakao.svg",s="/meta_test/imgs/btn_naver.png";export{t as _,s as a};
